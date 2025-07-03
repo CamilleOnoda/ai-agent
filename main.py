1884b6c5df8e86f2d9b59edaaeadee69a435df93
Camille Onoda <info@camilleonoda.com>
2025-07-03 16:01:21 +0900

    Add an optional command line flag '--verbose'

--- main.py
+++ main.py
@@ -14,20 +14,25 @@ def main():
         print("\nExample: python3 main.py 'Why the sky is blue?'")
         sys.exit(1)
     user_prompt = sys.argv[1]
-
-    generate_content(user_prompt, client)
-
-
-def generate_content(user_prompt, client):
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    generate_content(user_prompt, messages, client)
+
+
+def generate_content(user_prompt, messages, client):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages
         )
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    flag = "--verbose"
+
     print(f"Response :")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 2 and sys.argv[2] == flag:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
