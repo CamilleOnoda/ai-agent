14ebe4c9b5d0bd78590410d117601f03e6375c72
Camille Onoda <info@camilleonoda.com>
2025-07-10 18:00:46 +0900

    Build the declaration/schema for get_files_info() function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,24 @@
 from pathlib import Path
-
+from google import genai
+from google.genai import types
+            
 
 def get_files_info(working_directory, directory=None):
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+
     try:
         path_working_directory = Path(working_directory).resolve()
         if directory is None:
@@ -32,7 +49,8 @@ def get_files_info(working_directory, directory=None):
             else:
                 header = f"Result for {directory} directory:"
             return f"{header}\n{description}"
-
-
+        
     except Exception as e:
         return(f"Error: {e}")
+    
+
