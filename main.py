c5eb66a675f18f485f391e4c9adec73ab62f532a
Camille Onoda <info@camilleonoda.com>
2025-07-11 16:34:10 +0900

    Update the system prompt, list the available operations the agent can perform, specify the available functions the agent can use and print the function(s) called based on the user's prompt.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import os, sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python import schema_run_python_file
+from functions.write_file import schema_write_file
+
 
 def main():
     load_dotenv()
@@ -15,20 +20,50 @@ def main():
         sys.exit(1)
     user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    generate_content(user_prompt, messages, client)
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
+        ]
+    )
+
+    generate_content(user_prompt, messages, client, system_prompt, available_functions)
 
 
-def generate_content(user_prompt, messages, client):
+def generate_content(user_prompt, messages, client, system_prompt, available_functions):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+            )
         )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
     flag = "--verbose"
+    
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}{function_call.args}")       
+    else:
+        print(f"Response :")
+        print(f"{response.text}")
 
-    print(f"Response :")
-    print(response.text)
     if len(sys.argv) > 2 and sys.argv[2] == flag:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
